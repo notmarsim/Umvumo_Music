247109f2ae38a2cb3311018d7aa720e8d131a68f
Márcio <mhrmedeirosfilho@gmail.com>
2025-11-03 21:49:12 -0300

    alguns códigos ai

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import os
+import numpy as np
+import pandas as pd
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+import seaborn as sns
+import plotly.express as px
+import matplotlib.pyplot as plt
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from sklearn.metrics import euclidean_distances
+from scipy.spatial.distance import cdist
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import warnings
+warnings.filterwarnings("ignore")
+
+data = pd.read_csv("csv/data.csv")
+genre_data = pd.read_csv('csv/data_w_genres.csv')
+
+print(data.info())
+print(genre_data.info())
+
+top10_genres = genre_data.nlargest(10, 'popularity')
+
+fig = px.bar(top10_genres, x='genres', y=['valence', 'energy', 'danceability', 'acousticness'], barmode='group')
+fig.show()
+
+
+
+
+
+cluster_pipeline = Pipeline([('scaler', StandardScaler()), ('kmeans', KMeans(n_clusters=10, random_state=42))])
+X = genre_data.select_dtypes(np.number)
+cluster_pipeline.fit(X)
+genre_data['cluster'] = cluster_pipeline.predict(X)
+
+tsne_pipeline = Pipeline([('scaler', StandardScaler()), ('tsne', TSNE(n_components=2, verbose=1))])
+genre_embedding = tsne_pipeline.fit_transform(X)
+projection = pd.DataFrame(columns=['x', 'y'], data=genre_embedding)
+projection['genres'] = genre_data['genres']
+projection['cluster'] = genre_data['cluster']
+
+fig = px.scatter(
+    projection, x='x', y='y', color='cluster', hover_data=['x', 'y', 'genres'])
+fig.show()
