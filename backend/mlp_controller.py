068b2c2346fcfd00565ae78d77f73488dcc7eba1
Ian <iantpimentel@gmail.com>
2025-11-23 18:04:34 -0300

    pré-processamento de tabelas

--- backend/mlp_controller.py
+++ backend/mlp_controller.py
@@ -9,27 +9,29 @@ import warnings
 class MLP:
     def __init__(self):
         self.rna = None
+        self.rna_genres = None
         self.hidden_neurons = 40
         self.learning_rates = 0.001
         self.activation = 'tanh'
         self.tracks_full = pd.read_csv("csv/data_with_genres_id.csv")
-        self.tracks = self.setTracks()
+        self.genres = pd.read_csv('csv/data_by_genres.csv')
+        self.tracks = pd.read_csv("csv/normalized_data.csv")
         
 
-    def setTracks(self):
+    # def setTracks(self):
 
-        features = [
-            'valence', 'acousticness', 'danceability', 'duration_ms', 'energy',
-            'instrumentalness', 'liveness', 'loudness', 'speechiness', 'popularity', 'id_genre', 'id_artist'
-        ]
-        data = self.tracks_full[features]
+    #     features = [
+    #         'valence', 'acousticness', 'danceability', 'duration_ms', 'energy',
+    #         'instrumentalness', 'liveness', 'loudness', 'speechiness', 'popularity', 'id_genre', 'id_artist'
+    #     ]
+    #     data = self.tracks_full[features]
 
-        scaler = StandardScaler()
-        data_scaled = scaler.fit_transform(data)
+    #     scaler = StandardScaler()
+    #     data_scaled = scaler.fit_transform(data)
 
-        tracks = pd.DataFrame(data_scaled, columns=features)
+    #     tracks = pd.DataFrame(data_scaled, columns=features)
 
-        return tracks
+    #     return tracks
 
     def run(self,liked,disliked):
         self.train(liked,disliked)
@@ -38,24 +40,40 @@ class MLP:
     def train(self,liked,disliked):
 
         tracks_likes = self.tracks.copy().sort_values('popularity', ascending=False)
-        tracks_likes['like'] = self.tracks_full['id'].isin(liked).astype(int) - 100*self.tracks_full['id'].isin(disliked).astype(int)
+        # genre_likes = self.genres.copy()
+
+        tracks_likes['like'] = 1*self.tracks_full['id'].isin(liked).astype(int) - 100*self.tracks_full['id'].isin(disliked).astype(int)
+
         tracks_likes['order'] = tracks_likes['like'].apply(lambda x: 1 if x == 0 else 0)
         tracks_likes = tracks_likes.sort_values('order').drop('order',axis=1)
+
+
         
         train_ratio = 0.6
-        valid_ratio = 0.2
+        # valid_ratio = 0.2
 
         train_i = int(train_ratio * len(tracks_likes))
-        valid_i = int(valid_ratio * len(tracks_likes))
+        # valid_i = int(valid_ratio * len(tracks_likes))
 
         train_input = tracks_likes.iloc[:train_i, :-1]
         train_target = tracks_likes.iloc[:train_i, -1]
 
-        valid_input = tracks_likes.iloc[train_i:train_i + valid_i, :-1]
-        valid_target = tracks_likes.iloc[train_i:train_i + valid_i, -1]
+        # genre_train_i = int(train_ratio * len(tracks_likes))
 
-        # test_input = tracks_likes.iloc[train_i + valid_i:, :-1]
-        # test_target = tracks_likes.iloc[train_i + valid_i:, -1]
+        # genre_train_input = tracks_likes.iloc[:train_i, :-1]
+        # genre_train_target = tracks_likes.iloc[:train_i, -1]
+
+        # valid_input = tracks_likes.iloc[train_i:train_i + valid_i, :-1]
+        # valid_target = tracks_likes.iloc[train_i:train_i + valid_i, -1]
+        
+        # rna_genres = MLPClassifier(
+        #     hidden_layer_sizes=(self.hidden_neurons,),
+        #     learning_rate_init=self.learning_rates,
+        #     activation=self.activation,
+        #     solver='adam',
+        #     max_iter=500,
+        #     shuffle=False
+        # )
 
         rna = MLPClassifier(
             hidden_layer_sizes=(self.hidden_neurons,),
--- backend/test.py
+++ backend/test.py
@@ -1,41 +1,62 @@
 import pandas as pd
-import ast
-df = pd.read_csv('csv/data_with_genres.csv')
+import numpy as np
+import random
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+import warnings
 
-genre = pd.read_csv('csv/data_by_genres.csv')
-genre.index = genre['genres']
-df['id_genre'] = [0]*170653
+# df = pd.read_csv('csv/data_with_genres.csv')
 
-def create_genre_ids(genres):
+# genre = pd.read_csv('csv/data_by_genres.csv')
+# genre.index = genre['genres']
+# df['id_genre'] = [0]*170653
+
+# def create_genre_ids(genres):
     
-    artist1_genres = eval(genres)
-    if artist1_genres != []: artist1_genres = artist1_genres[0]
-    if artist1_genres in ['[]',[]]:
-        return genre.loc['[]']['id_genre']
-    else:
-        artist1_main_genre = eval(artist1_genres)[0]
-        return genre.loc[artist1_main_genre]['id_genre']
+#     artist1_genres = eval(genres)
+#     if artist1_genres != []: artist1_genres = artist1_genres[0]
+#     if artist1_genres in ['[]',[]]:
+#         return genre.loc['[]']['id_genre']
+#     else:
+#         artist1_main_genre = eval(artist1_genres)[0]
+#         return genre.loc[artist1_main_genre]['id_genre']
 
-print('genres')
-df['id_genre'] = df['genres'].apply(create_genre_ids)
+# print('genres')
+# df['id_genre'] = df['genres'].apply(create_genre_ids)
 
-df.to_csv('csv/data_with_genres_id.csv')
-
-
-artist = pd.read_csv('csv/data_w_genres.csv').sort_values('id_artist')
-artist.index = artist['artists']
-df = pd.read_csv('csv/data_with_genres_id.csv')
-
-def create_artist_id(artists):
-    global i
-    main_artist = eval(artists)[0]
-    if main_artist == 'n/a':
-        return -1 
-    return artist.loc[main_artist]['id_artist']
+# df.to_csv('csv/data_with_genres_id.csv')
 
 
-print('artists')
-df['id_artist'] = df['artists'].apply(create_artist_id)
+# artist = pd.read_csv('csv/data_w_genres.csv').sort_values('id_artist')
+# artist.index = artist['artists']
+# df = pd.read_csv('csv/data_with_genres_id.csv')
 
-df.to_csv('csv/data_with_genres_id.csv')
+# def create_artist_id(artists):
+#     global i
+#     main_artist = eval(artists)[0]
+#     if main_artist == 'n/a':
+#         return -1 
+#     return artist.loc[main_artist]['id_artist']
 
+
+# print('artists')
+# df['id_artist'] = df['artists'].apply(create_artist_id)
+
+# df.to_csv('csv/data_with_genres_id.csv')
+
+
+df = pd.read_csv('csv/data_by_genres.csv')
+
+features = [
+    'acousticness','danceability','duration_ms','energy','instrumentalness','liveness','loudness','speechiness','valence','popularity'
+]
+
+data = df[features]
+
+scaler = StandardScaler()
+data_scaled = scaler.fit_transform(data)
+
+tracks = pd.DataFrame(data_scaled, columns=features)
+
+tracks.to_csv('csv/normalized_genres.csv')
